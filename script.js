da2686547dbf8d30156880373049403dd8ced3be
Łukasz Szerszyński <lukiszerszen@gmail.com>
2023-06-22 23:02:16 +0200

    makes code more cleary

--- script.js
+++ script.js
@@ -1,23 +1,40 @@
-let todoInput, newTodo, ulList, newTodos;
 
-const main = () => {
-    prepareDOMElementsAndEvents ()
-}
-const prepareDOMElementsAndEvents = () => {
-    todoInput = document.querySelector('.todo-input')
-    ulList = document.querySelector('.todolist ul')
-    todoInput.addEventListener('keyup', addNewTodo)
-}
-// Dodajemy nowego todo jezeli input nie jest pusty i uzyjemy entera. tworzymy wtedy li z naszej ullisty i dodajemy do niej tekst ktory wpisalismy w inpucie. Nastepnie chcemy by ten tekst zniknal z inputa
-const addNewTodo = (e) => {
-    if (todoInput.value !== '' && e.key === 'Enter') {
-        newTodo = document.createElement('li');
-        newTodo.textContent = todoInput.value
-        ulList.append(newTodo)
-        todoInput.value = ''
+// prepare DOM Elements
+    const todoInput = document.querySelector('.todo-input')
+    const ulList = document.querySelector('.todolist ul')
+    const deleteBtn = document.querySelector('.delete')
+    const complete = document.querySelector('.complete1')
+    
+    // This function adds new todo, after press enter. 
+    const addNewTodo = (e) => {
+        if (todoInput.value !== '' && e.key === 'Enter') {
+            const newTodo = document.createElement('li');
+            newTodo.textContent = todoInput.value
+            // its should add checkbox and deleteBtn
+            createToolsArea(newTodo)
+             ulList.append(newTodo)
+            todoInput.value = ''
+        }
     }
+    // prepare DOM event
+    todoInput.addEventListener('keyup', addNewTodo)
+    
+const createToolsArea = (newTodo) => {
+    const checkbox = document.createElement ('checkbox')
+    checkbox.classList.add('complete1')
+    newTodo.append(checkbox)
+    
+    const toolsPanel = document.createElement ('div')
+    toolsPanel.classList.add('tools')
+    newTodo.append(toolsPanel)
+    
+    const deleteBtn = document.createElement ('button')
+    deleteBtn.classList.add('delete')
+    deleteBtn.innerHTML = '<i class="fas fa-times"></i>'
+    
+    toolsPanel.append(deleteBtn)
+    checkbox.append(complete)
 }
 
 
-
-document.addEventListener('DOMContentLoaded',main)
+document.addEventListener('DOMContentLoaded')
