a8181d3521e00d20e4b79222671f24b96b6f3669
Łukasz <130897150+Szersz87@users.noreply.github.com>
2023-06-27 20:40:49 +0200

    Enter input (#2)
    * import class to js
    
    * enter to add new todos
    
    * changes in JS code
    
    * makes code more cleary

--- script.js
+++ script.js
@@ -1,20 +1,40 @@
-let todoInput, newTodo, ulList, completeBtn, deleteBtn, allBtn, activeBtn,completedBtn;
 
-const main = () => {
-    prepareDOMElements ()
-    prepareDOMEvents ()
+// prepare DOM Elements
+    const todoInput = document.querySelector('.todo-input')
+    const ulList = document.querySelector('.todolist ul')
+    const deleteBtn = document.querySelector('.delete')
+    const complete = document.querySelector('.complete1')
+    
+    // This function adds new todo, after press enter. 
+    const addNewTodo = (e) => {
+        if (todoInput.value !== '' && e.key === 'Enter') {
+            const newTodo = document.createElement('li');
+            newTodo.textContent = todoInput.value
+            // its should add checkbox and deleteBtn
+            createToolsArea(newTodo)
+             ulList.append(newTodo)
+            todoInput.value = ''
+        }
+    }
+    // prepare DOM event
+    todoInput.addEventListener('keyup', addNewTodo)
+    
+const createToolsArea = (newTodo) => {
+    const checkbox = document.createElement ('checkbox')
+    checkbox.classList.add('complete1')
+    newTodo.append(checkbox)
+    
+    const toolsPanel = document.createElement ('div')
+    toolsPanel.classList.add('tools')
+    newTodo.append(toolsPanel)
+    
+    const deleteBtn = document.createElement ('button')
+    deleteBtn.classList.add('delete')
+    deleteBtn.innerHTML = '<i class="fas fa-times"></i>'
+    
+    toolsPanel.append(deleteBtn)
+    checkbox.append(complete)
 }
-const prepareDOMElements = () => {
-    todoInput = document.querySelector('.todo-input')
-    add = document.querySelector('.')
-    ulList = document.querySelector('.todolist ul')
-    deleteBtn = document.querySelector('.delete')
-    completeBtn = document.querySelector('.complete')
-    allBtn = document.querySelector('.btn-all')
-    activeBtn = document.querySelector('.btn-active')
-    completedBtn = document.querySelector('.btn-completed')
-}
-const prepareDOMEvents = () => {
 
-}
-document.addEventListener('DOMContentLoaded',main)
+
+document.addEventListener('DOMContentLoaded')
