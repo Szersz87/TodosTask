a00ec6783e2ebba4d9867a264cb38108d8f6a60d
Łukasz <130897150+Szersz87@users.noreply.github.com>
2023-07-04 22:05:34 +0200

    Todos panel activation (#4)
    * import class to js
    
    * enter to add new todos
    
    * changes in JS code
    
    * makes code more cleary
    
    * creat todo panel
    
    * first function of toolsPanel
    
    * more visible js structure
    
    * change funkcion from if/else to switch/case
    
    * all tools active
    
    * panel active

--- script.js
+++ script.js
@@ -1,40 +1,98 @@
-
 // prepare DOM Elements
-    const todoInput = document.querySelector('.todo-input')
-    const ulList = document.querySelector('.todolist ul')
-    const deleteBtn = document.querySelector('.delete')
-    const complete = document.querySelector('.complete1')
-    
-    // This function adds new todo, after press enter. 
-    const addNewTodo = (e) => {
-        if (todoInput.value !== '' && e.key === 'Enter') {
-            const newTodo = document.createElement('li');
-            newTodo.textContent = todoInput.value
-            // its should add checkbox and deleteBtn
-            createToolsArea(newTodo)
-             ulList.append(newTodo)
-            todoInput.value = ''
-        }
-    }
-    // prepare DOM event
-    todoInput.addEventListener('keyup', addNewTodo)
-    
-const createToolsArea = (newTodo) => {
-    const checkbox = document.createElement ('checkbox')
-    checkbox.classList.add('complete1')
-    newTodo.append(checkbox)
-    
-    const toolsPanel = document.createElement ('div')
-    toolsPanel.classList.add('tools')
-    newTodo.append(toolsPanel)
-    
-    const deleteBtn = document.createElement ('button')
-    deleteBtn.classList.add('delete')
-    deleteBtn.innerHTML = '<i class="fas fa-times"></i>'
-    
-    toolsPanel.append(deleteBtn)
-    checkbox.append(complete)
-}
+const todoInput = document.getElementsByClassName("todo-input")[0];
+const ulList = document
+  .getElementsByClassName("todolist")[0]
+  .getElementsByTagName("ul")[0];
 
+// This function adds new todo, after press enter.
+const addNewTodo = (e) => {
+  if (todoInput.value.trim() !== "" && e.key === "Enter") {
+    const newLi = document.createElement("li");
+    createToolsArea(newLi, todoInput.value);
+    ulList.append(newLi);
+    todoInput.value = "";
+  }
+};
+// prepare DOM event
+todoInput.addEventListener("keyup", addNewTodo);
 
-document.addEventListener('DOMContentLoaded')
+const createElementWithClass = (type, className) => {
+  const element = document.createElement(type);
+  element.classList.add(className);
+  return element;
+};
+
+const createToolsArea = (newTodo, text) => {
+  newTodo.innerHTML = `
+  <div class='tools'>
+    <div class='circle'></div>
+    <p class='textArea'>${text}</p>
+    <button class='delete'><i class="fas fa-times"></i></button>
+  </div>
+`;
+};
+const handleDoubleClick = (e) => {
+  const listItem = e.target.closest("li");
+  const checkBox = listItem.querySelector(".circle");
+  const textArea = listItem.querySelector(".textArea");
+
+  if (e.target.classList.contains("textArea")) {
+    textArea.contentEditable = true;
+    textArea.focus();
+  }
+};
+const handleKeyDown = (e) => {
+  const textArea = e.target.closest(".textArea");
+  if (e.key === "Enter" && textArea?.contentEditable === "true") {
+    e.preventDefault();
+    textArea.contentEditable = false;
+  }
+};
+const checkClick = (e) => {
+  const listItem = e.target.closest("li");
+  const checkBox = listItem.querySelector(".circle");
+  const textArea = listItem.querySelector(".textArea");
+  switch (true) {
+    case e.target.classList.contains("circle"):
+      listItem.classList.toggle("completed");
+      if (listItem.classList.contains("completed")) {
+        checkBox.innerHTML = '<i class="fas fa-check"></i>';
+      } else {
+        checkBox.innerHTML = "";
+      }
+      break;
+    case e.target.classList.contains("delete"):
+      e.target.closest("li").remove();
+      break;
+  }
+};
+const eventHandlers = {
+  click: checkClick,
+  dblclick: handleDoubleClick,
+  keydown: handleKeyDown,
+};
+["click", "dblclick", "keydown"].forEach((event) => {
+  ulList.addEventListener(event, eventHandlers[event]);
+});
+
+// Create eventHandlers object
+// prepare btns for used
+const buttons = document.querySelectorAll(
+  ".btn-all, .btn-active, .btn-completed"
+);
+// funktion looking for btns in clg
+const handleClick = (e) => {
+  const clickedButton = e.target;
+
+  if (clickedButton.classList.contains("btn-all")) {
+    console.log("Clicked btn-all");
+  } else if (clickedButton.classList.contains("btn-active")) {
+    console.log("Clicked btn-active");
+  } else if (clickedButton.classList.contains("btn-completed")) {
+    console.log("Clicked btn-completed");
+  }
+};
+// lisener for click on bts
+buttons.forEach((button) => {
+  button.addEventListener("click", handleClick);
+});
