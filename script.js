124b39086756bf5cadf322583b19f7b55144bae6
Łukasz Szerszyński <lukiszerszen@gmail.com>
2023-07-04 21:57:21 +0200

    panel active

--- script.js
+++ script.js
@@ -1,82 +1,98 @@
-
 // prepare DOM Elements
-    const todoInput = document.querySelector('.todo-input')
-    const ulList = document.querySelector('.todolist ul')
-    // const deleteBtn = document.querySelector('.delete')
-    // const circle = document.querySelector('.circle')
-    // const check = document.querySelector('.fas fa-check')
-    
-    
-    // This function adds new todo, after press enter. 
-    const addNewTodo = (e) => {
-        if (todoInput.value !== '' && e.key === 'Enter') {
-            const newLi = document.createElement('li');
-            createToolsArea(newLi, todoInput.value)
-             ulList.append(newLi)
-            todoInput.value = ''
-        }
-    }
-    // prepare DOM event
-    todoInput.addEventListener('keyup', addNewTodo)
-    
-    const createElementWithClass = (type, className) => {
-                const element = document.createElement(type);
-                element.classList.add(className);
-                return element;
-            }
+const todoInput = document.getElementsByClassName("todo-input")[0];
+const ulList = document
+  .getElementsByClassName("todolist")[0]
+  .getElementsByTagName("ul")[0];
 
-        const createToolsArea = (newTodo, text) => {
+// This function adds new todo, after press enter.
+const addNewTodo = (e) => {
+  if (todoInput.value.trim() !== "" && e.key === "Enter") {
+    const newLi = document.createElement("li");
+    createToolsArea(newLi, todoInput.value);
+    ulList.append(newLi);
+    todoInput.value = "";
+  }
+};
+// prepare DOM event
+todoInput.addEventListener("keyup", addNewTodo);
 
-                const toolsPanel = createElementWithClass('div', 'tools');
-                const checkBox = createElementWithClass('circle', 'circle');
-                const textArea = createElementWithClass('p', 'textArea');
-                const deleteBtn = createElementWithClass('button', 'delete');
-                deleteBtn.innerHTML = '<i class="fas fa-times"></i>';
-        textArea.textContent = text
-                toolsPanel.append(checkBox, textArea, deleteBtn);
-                newTodo.append(toolsPanel);
-        }
-// funkcja doubleClicka, jesli klikniemy na tekst w divie w li, uruchomimy edycje tekstu
+const createElementWithClass = (type, className) => {
+  const element = document.createElement(type);
+  element.classList.add(className);
+  return element;
+};
+
+const createToolsArea = (newTodo, text) => {
+  newTodo.innerHTML = `
+  <div class='tools'>
+    <div class='circle'></div>
+    <p class='textArea'>${text}</p>
+    <button class='delete'><i class="fas fa-times"></i></button>
+  </div>
+`;
+};
 const handleDoubleClick = (e) => {
-            const listItem = e.target.closest('li');
-            const checkBox = listItem.querySelector('.circle');
-            const textArea = listItem.querySelector('.textArea');
-          
-            if (e.target.classList.contains('textArea')) {
-              textArea.contentEditable = true;
-              textArea.focus();
-            }
-          };
-        //   funkcja zamykania okna edycji po kliknieciu entera
- const handleKeyDown = (e) => {
-         const textArea = e.target.closest('.textArea');
-            if (e.key === 'Enter' && textArea && textArea.contentEditable === 'true') {
-              e.preventDefault();
-              textArea.contentEditable = false;
-            }
-          };
-const checkClick = e => {
-    const listItem = e.target.closest('li');
-    const checkBox = listItem.querySelector('.circle');
-    const textArea = listItem.querySelector('.textArea');
-  
-    switch (true) {
-        // jesli klikniemy na diva circle dodamy mu ikonke checked, a textarea otrzyma style comleted
-        case e.target.classList.contains('circle'):
-            listItem.classList.toggle('completed');
-            if (listItem.classList.contains('completed')) {
-                checkBox.innerHTML = '<i class="fas fa-check"></i>';
-            } else {
-                checkBox.innerHTML = '';
-            }
-            break;
-        case e.target.classList.contains('delete'):
-            e.target.closest('li').remove();
-            break;
-    }
-}
+  const listItem = e.target.closest("li");
+  const checkBox = listItem.querySelector(".circle");
+  const textArea = listItem.querySelector(".textArea");
 
-ulList.addEventListener('click', checkClick);
-ulList.addEventListener('dblclick', handleDoubleClick);
-ulList.addEventListener('keydown', handleKeyDown);
+  if (e.target.classList.contains("textArea")) {
+    textArea.contentEditable = true;
+    textArea.focus();
+  }
+};
+const handleKeyDown = (e) => {
+  const textArea = e.target.closest(".textArea");
+  if (e.key === "Enter" && textArea?.contentEditable === "true") {
+    e.preventDefault();
+    textArea.contentEditable = false;
+  }
+};
+const checkClick = (e) => {
+  const listItem = e.target.closest("li");
+  const checkBox = listItem.querySelector(".circle");
+  const textArea = listItem.querySelector(".textArea");
+  switch (true) {
+    case e.target.classList.contains("circle"):
+      listItem.classList.toggle("completed");
+      if (listItem.classList.contains("completed")) {
+        checkBox.innerHTML = '<i class="fas fa-check"></i>';
+      } else {
+        checkBox.innerHTML = "";
+      }
+      break;
+    case e.target.classList.contains("delete"):
+      e.target.closest("li").remove();
+      break;
+  }
+};
+const eventHandlers = {
+  click: checkClick,
+  dblclick: handleDoubleClick,
+  keydown: handleKeyDown,
+};
+["click", "dblclick", "keydown"].forEach((event) => {
+  ulList.addEventListener(event, eventHandlers[event]);
+});
 
+// Create eventHandlers object
+// prepare btns for used
+const buttons = document.querySelectorAll(
+  ".btn-all, .btn-active, .btn-completed"
+);
+// funktion looking for btns in clg
+const handleClick = (e) => {
+  const clickedButton = e.target;
+
+  if (clickedButton.classList.contains("btn-all")) {
+    console.log("Clicked btn-all");
+  } else if (clickedButton.classList.contains("btn-active")) {
+    console.log("Clicked btn-active");
+  } else if (clickedButton.classList.contains("btn-completed")) {
+    console.log("Clicked btn-completed");
+  }
+};
+// lisener for click on bts
+buttons.forEach((button) => {
+  button.addEventListener("click", handleClick);
+});
