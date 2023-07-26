3c0a1a08f55727967ea082231a017f3193dd9cb8
Łukasz <130897150+Szersz87@users.noreply.github.com>
2023-07-26 21:11:21 +0200

    Array statred (#8)
    * merge
    
    * creat todos in array
    
    * delete btn
    
    * delete in array and DOM
    
    * some style changes
    
    * debug newtodo
    
    * completed
    
    * optymalized

--- script.js
+++ script.js
@@ -1,164 +1,207 @@
-// prepare DOM Elements
+document.addEventListener("DOMContentLoaded", () => {
+const todoInput = document.querySelector(".todo-input");
+const ulList = document.querySelector(".todolist ul");
+const buttons = document.querySelectorAll(
+  ".btn-all, .btn-active, .btn-completed, .btn-deleteAllCompleted"
+  );
+  
+  const tasks = [];
 
-    const todoInput = document.querySelector('.todo-input');
-    const ulList = document.querySelector('.todolist ul');
-    const buttons = document.querySelectorAll('.btn-all, .btn-active, .btn-completed, .btn-deleteAllCompleted');
-    const todoItems = document.querySelectorAll('.todolist ul li');
-    
-    // This function adds new todo, after press enter. 
-    const addNewTodo = (e) => {
-        if (todoInput.value !== '' && e.key === 'Enter') {
-            const newLi = document.createElement('li');
-            createToolsArea(newLi, todoInput.value)
-             ulList.append(newLi)
-            todoInput.value = ''
-        }
+  
+
+  // This function adds new todo, after press enter.
+  const addNewTodo = (e) => {
+    e.preventDefault();
+  
+    if (e.key === "Enter") {
+      const text = todoInput.value.trim();
+      if (text !== "") {
+        const newTodo = {
+          id: new Date().valueOf(),
+          text: text,
+          status: "created",
+        };
+  
+        tasks.push(newTodo);
+        createTodosArea(newTodo); 
+        todoInput.value = "";
+      }
     }
-    // prepare DOM event
-    todoInput.addEventListener('keyup', addNewTodo)
-    
-    const createElementWithClass = (type, className) => {
-                const element = document.createElement(type);
-                element.classList.add(className);
-                return element;
-            }
+  };
 
-        const createToolsArea = (newTodo, text) => {
 
-                const toolsPanel = createElementWithClass('div', 'tools');
-                const checkBox = createElementWithClass('div', 'circle');
-                const textArea = createElementWithClass('p', 'textArea');
-                const deleteBtn = createElementWithClass('button', 'delete');
-                deleteBtn.innerHTML = '<i class="fas fa-times"></i>';
-        textArea.textContent = text
-                toolsPanel.append(checkBox, textArea, deleteBtn);
-                newTodo.append(toolsPanel);
-        }
+const createElementWithClass = (type, className) => {
+  const element = document.createElement(type);
+  element.classList.add(className);
+  return element;
+};
+
+const createTodosArea = (task) => {
+  const newLi = document.createElement("li");
+  newLi.classList.add('newTodo');
+  newLi.setAttribute("data-id", task.id);
+  
+  newLi.innerHTML = `
+    <div class="tools">
+      <div class="circle"></div>
+      <p class="textArea">${task.text}</p>
+      <button class="delete"><i class="fas fa-times"></i></button>
+    </div>
+  `;
+
+  const deleteBtn = newLi.querySelector(".delete");
+  deleteBtn.addEventListener("click", () => {
+    deleteTodoAndElement(task.id, newLi);
+  });
+  ulList.append(newLi);
+  
+};
+const findTaskIndexById = (id) => {
+  return tasks.findIndex((task) => task.id === id);
+};
+
+
+const deleteTaskById = (id) => {
+  const index = findTaskIndexById(id);
+  if (index !== -1) {
+    tasks.splice(index, 1);
+  }
+};
+
+const deleteElement = (element) => {
+  element.remove();
+};
+
+const deleteTodoAndElement = (id, element) => {
+  deleteTaskById(id);
+  deleteElement(element);
+};
+
+
 
 // funkcja doubleClicka, jesli klikniemy na tekst w divie w li, uruchomimy edycje tekstu
-const handleDoubleClick = (e) => {
-            const listItem = e.target.closest('li');
-            const checkBox = listItem.querySelector('.circle');
-            const textArea = listItem.querySelector('.textArea');
-          
-            if (e.target.classList.contains('textArea')) {
-              textArea.contentEditable = true;
-              textArea.focus();
-            }
-          };
-        //   funkcja zamykania okna edycji po kliknieciu entera
- const handleKeyDown = (e) => {
-         const textArea = e.target.closest('.textArea');
-            if (e.key === 'Enter' && textArea && textArea.contentEditable === 'true') {
-              e.preventDefault();
-              textArea.contentEditable = false;
-            }
-          };
-const checkClick = e => {
-    
-    const listItem = e.target.closest('li');
-    const checkBox = listItem.querySelector('.circle');
-    const textArea = listItem.querySelector('.textArea');
-    switch (true) {
-        // jesli klikniemy na diva circle dodamy mu ikonke checked, a textarea otrzyma style comleted
-        case e.target.classList.contains('circle'):
-            listItem.classList.toggle('completed');
-            if (listItem.classList.contains('completed')) {
-                checkBox.innerHTML = '<i class="fas fa-check"></i>';
-            } else {
-                checkBox.innerHTML = '';
-            }
-            break;
-        case e.target.classList.contains('delete'):
-            e.target.closest('li').remove();
-            break;
-    }
-}
 
-ulList.addEventListener('click', checkClick);
-ulList.addEventListener('dblclick', handleDoubleClick);
-ulList.addEventListener('keydown', handleKeyDown);
+const handleDoubleClick = (e) => {
+  if (e.target.classList.contains("textArea")) {
+    e.target.contentEditable = true;
+    e.target.focus();
+  }
+};
+
+
+//   funkcja zamykania okna edycji po kliknieciu entera
+const handleKeyDown = (e) => {
+  if (e.key === "Enter" && e.target.contentEditable === "true") {
+    e.preventDefault();
+    e.target.contentEditable = false;
+  }
+};
+const checkClick = (e) => {
+  const listItem = e.target.closest("li");
+
+  if (e.target.classList.contains("circle")) {
+    listItem.classList.toggle("completed");
+    e.target.innerHTML = listItem.classList.contains("completed") ? '<i class="fas fa-check"></i>' : "";
+  }
+  else if (e.target.classList.contains("delete")) {
+    const todoId = listItem.dataset.id;
+    deleteTodoAndElement(todoId, listItem);
+  }
+};
+
+
+
 
 
 const handleClick = (e) => {
   const clickedButton = e.target;
 
-  switch (true) {
-    case clickedButton.classList.contains("btn-all"):
-      showAllTodos();
-      break;
-    case clickedButton.classList.contains("btn-active"):
+      case clickedButton.classList.contains("btn-active"):
       showActiveTodos();
       break;
-    case clickedButton.classList.contains("btn-completed"):
+      case clickedButton.classList.contains("btn-completed"):
       showCompletedTodos();
       break;
-    case clickedButton.classList.contains("btn-deleteAllCompleted"):
-      deleteCompletedTodos();
-      break;
-      default:
+      case clickedButton.classList.contains("btn-deleteAllCompleted"):
+        deleteCompletedTodos();
         break;
-      }
-    };
-    
-buttons.forEach((button) => {
-  button.addEventListener("click", handleClick);
-});
+        default:
+          break;
+        }
+      };
 
-// shows all todos
-const showAllTodos = () => {
-  const todoItems = document.querySelectorAll('.todolist ul li');
-  todoItems.forEach((item) => {
-    item.style.display = "block";
-  });
-};
+      
+      const applyToTodos = (condition, trueAction, falseAction) => {
+        const todoItems = ulList.querySelectorAll("li[data-id]");
+        todoItems.forEach((item) => {
+          if (condition(item)) {
+            trueAction(item);
+          } else {
+            falseAction(item);
+          }
+        });
+      };
+      
+      const showAllTodos = () => {
+        applyToTodos(
+          () => true,
+          (listItem) => {
+            listItem.style.display = "block";
+          },
+        );
+      };
+      
+      const showActiveTodos = () => {
+        applyToTodos(
+          (item) => !item.classList.contains("completed"),
+          (listItem) => {
+            listItem.style.display = "block";
+          },
+          (listItem) => {
+            listItem.style.display = "none";
+          }
+        );
+      };
+      
+      const showCompletedTodos = () => {
+        applyToTodos(
+          (item) => item.classList.contains("completed"),
+          (listItem) => {
+            listItem.style.display = "block";
+          },
+          (listItem) => {
+            listItem.style.display = "none";
+          }
+        );
+      };
+      
+      const deleteCompletedTodos = () => {
+        applyToTodos(
+          (item) => item.classList.contains("completed"),
+          (listItem) => {
+            listItem.remove();
+          },
+          () => {}
+        );
+      };
+      
+      
+      
 
-// shows todos without class completed
-const showActiveTodos = () => {
-  const todoItems = document.querySelectorAll('.todolist ul li');
-  todoItems.forEach((item) => {
-    if (!item.classList.contains("completed")) {
-      item.style.display = "block";
-    } else {
-      item.style.display = "none";
-    }
-  });
-};
+      const elementsEvents = [
+        { elements: [todoInput], event: "keyup", handler: addNewTodo },
+        { elements: [ulList], event: "click", handler: checkClick },
+        { elements: [ulList], event: "dblclick", handler: handleDoubleClick },
+        { elements: [ulList], event: "keydown", handler: handleKeyDown },
+        { elements: buttons, event: "click", handler: handleClick },
+      ];
+      const addEventListeners = (elements, event, handler) => {
+        elements.forEach((element) => {
+          element.addEventListener(event, handler);
+        });
+      };
+      elementsEvents.forEach(({ elements, event, handler }) => {
+        addEventListeners(elements, event, handler);
+      });
+    })
 
-// shows todos with class completed
-const showCompletedTodos = () => {
-  const todoItems = document.querySelectorAll('.todolist ul li');
-  todoItems.forEach((item) => {
-    if (item.classList.contains("completed")) {
-      item.style.display = "block";
-    } else {
-      item.style.display = "none";
-    }
-  });
-};
-
-
-// delete all completed todos
-const deleteCompletedTodos = () => {
-  const todoItems = document.querySelectorAll('.todolist ul li');
-
-  todoItems.forEach((item) => {
-    if (item.classList.contains("completed")) {
-      item.remove();
-      } else {
-        item.style.display = "block";
-    }
-  });
-  deleteCompletedButtonVisibility();
-}
-// show deleteAll button when something will be completed
-const deleteCompletedButtonVisibility = () => {
-  const deleteButton = document.querySelector('.btn-deleteAllCompleted');
-  const completedTodos = document.querySelectorAll('.completed');
-
-  if (completedTodos.length > 0) {
-    deleteButton.style.display = 'block';
-  } else {
-    deleteButton.style.display = 'none';
-  }
-};
